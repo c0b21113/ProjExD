09a3422ff00cefeed2d703ba7801c9ed3628faab
tsuda misaki <c0b211134b@edu.teu.ac.jp>
2022-10-04 14:48:08 +0900

    電卓　第２回 2回目

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn=event.widget
+    txt=btn["text"]
+    tkm.showinfo(txt,f"[{txt}ボタンが押されました]")
+
 
 
 root=tk.Tk()
@@ -9,6 +16,7 @@ count=0
 
 for i in range(9,-1,-1):
     button=tk.Button(root,text=f"{i}",width=4,height=2,font=("Times New Roman",30))
+    button.bind("<1>",button_click)
     button.grid(row=R,column=C)
     count+=1
     if C==2:
